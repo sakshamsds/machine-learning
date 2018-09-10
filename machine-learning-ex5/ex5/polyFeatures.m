4666f516f41869e88540f1ed707e8aff0359c267
Sakshamdeep Singh <42541692+sakshamsds@users.noreply.github.com>
2018-09-10 15:03:04 +0530

    Update polyFeatures.m

--- machine-learning-ex5/ex5/polyFeatures.m
+++ machine-learning-ex5/ex5/polyFeatures.m
@@ -14,11 +14,11 @@ X_poly = zeros(numel(X), p);
 %               column of X contains the values of X to the p-th power.
 %
 % 
+X_poly = X;
 
-
-
-
-
+for i=1:p
+    X_poly(:,i) = X.^i;     
+end
 
 % =========================================================================
 
